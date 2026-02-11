24ee2f722b6354e75b96793891b965d7d6b75b62
Wender Machado <devwendermachado@gmail.com>
2026-02-11 20:34:05 -0300

    criando estilo básico para crud simples

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
- 
+ function createTransaction(transactions){
+    const cardTransaction = document.createElement(`div`)
+    cardTransaction.classList.add(`balance`)
+    cardTransaction.id = (`transaction-${transactions.id}`)
+
+    const client = document.createElement(`span`)
+    client.classList.add(`title`)
+    client.innerText = `${transactions.name}`
+
+    cardTransaction.append(client)
+    document.querySelector(`#transactions`).append(cardTransaction)
+ }
+
+ const renderTransactions = async function(){
+   
+    const response = await fetch("http://localhost:3000/finatials")
+    const data = await response.json()
+
+      data.forEach(createTransaction)
+ }
+
+ document.addEventListener(`DOMContentLoaded`, ()=>{
+    renderTransactions()
+})
